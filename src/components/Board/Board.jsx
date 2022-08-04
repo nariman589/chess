fd246d9507693a594e1d15c5e1d30fc690d71455
nariman589 <Doctorr01@yandex.ru>
2022-08-04 22:44:45 +0600

    added board

---
+++ src/components/Board/Board.jsx
@@ -0,0 +1,24 @@
+import "./index.css";
+
+export default function Board() {
+  const board = [];
+  let flag = true;
+  for (let i = 1; i <= 8; i++) {
+    for (let j = 1; j <= 8; j++) {
+      if (j === 1) flag = !flag;
+      if (flag) board.push(<div className="black ceil"></div>);
+      else board.push(<div className="white ceil"></div>);
+      flag = !flag;
+    }
+
+    // board.push(<div className="black ceil"></div>)
+    // board.push(<div className="white ceil"></div>)
+  }
+  return (
+    <div className="board">
+      {board.map((ceil) => {
+        return ceil;
+      })}
+    </div>
+  );
+}
